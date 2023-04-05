bc20781d3fc19484640938869f6cd1797059f460
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-04-05 10:57:35 +0300

    feat: add profile command

--- src/handlers/command_handler.py
+++ src/handlers/command_handler.py
@@ -1,8 +1,10 @@
 import os
+from typing import Any
 from telegram import Update, Bot, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 import json
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.database import Database
 from misc import constants
 
 load_dotenv()
@@ -11,6 +13,8 @@ load_dotenv()
 class CommandHandler:
     bot: Bot
     update: Update
+    chat_id: int
+    db: Database
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
@@ -18,15 +22,20 @@ class CommandHandler:
     async def decider(self, update: Update):
         # Decide where to go
         self.update = update
-        command = update.message.text
+        self.chat_id = update.message.chat_id
+        text = update.message.text
+        client = MongoClient(os.getenv('MONGO_CONNECTION_URL'))
+        self.db = client.telegram
 
-        match command:
+        match text:
             case '/start':
                 await self.start()
             case '/help':
                 await self.help()
             case '/new_post':
                 await self.new_post()
+            case '/profile':
+                await self.profile()
             case _:
                 await self.default()
 
@@ -38,15 +47,22 @@ class CommandHandler:
     async def start(self):
         id = self.update.message.from_user.id
         text = f"""Hello there <b>{self.update.message.from_user.first_name}</b>, welcome to the <b>Telegram Content Management</b> bot. \
-            Here are some of the things you can do with this bot:
-        - Upload a post
+            I can help you manage your contents across different groups and channels.\n\nYou can control me by sending these commands:
         """
+        with open('config/commands.json') as commands_file:
+            commands_json = json.load(commands_file)
+
+            for command in commands_json["commands"]:
+                text += "\n/" + command['command'] + \
+                    " - " + command['description']
+
+        await self.register_user()
         await self.bot.send_message(text=text, chat_id=id, parse_mode="HTML")
 
     async def help(self):
         chat_id = self.update.message.from_user.id
         text = 'Need help with commands?\nHere are list of commands you can use:\n'
-        with open('src/misc/commands.json') as commands_file:
+        with open('config/commands.json') as commands_file:
             commands_json = json.load(commands_file)
 
             for command in commands_json["commands"]:
@@ -56,15 +72,39 @@ class CommandHandler:
         await self.bot.send_message(text=text, chat_id=chat_id, parse_mode="HTML")
 
     async def new_post(self):
-        client = MongoClient(os.getenv('MONGO_CONNECTION_URL'))
-        db = client['telegram']
-        sessions = db['sessions']
+        chat_id = self.update.message.from_user.id
 
-        sessions.update_one({"chat_id": self.update.message.chat_id}, {
+        self.db.sessions.update_one({"chat_id": chat_id}, {
             '$set': {"command": "/post", "question": constants.INSERT_POST_INFO, "data": {}}}, upsert=True)
 
-        chat_id = self.update.message.from_user.id
         text = f"""Using the following attributes, please add detail information about your post"""
         reply_markup = constants.NEW_POST_INLINE_KEYBOARD
 
         await self.bot.send_message(text=text, chat_id=chat_id, reply_markup=reply_markup, parse_mode="HTML")
+
+    async def register_user(self):
+        user_from_db = self.db.users.find_one({"chat_id": self.chat_id})
+        if user_from_db == None:
+            self.db.users.insert_one(
+                {
+                    "chat_id": self.chat_id,
+                    "name": self.update.message.from_user.first_name + " " + self.update.message.from_user.last_name,
+                    "username": self.update.message.from_user.username,
+                    "channels": [],
+                    "groups": [],
+                })
+
+    async def profile(self):
+        user_from_db = self.db.users.find_one({"chat_id": self.chat_id})
+        text = f"Your profile info:\n\nName: {user_from_db['name']}\nUsername: @{user_from_db['username']}"
+
+        if user_from_db['channels'] != []:
+            text += "\nChannels: "
+            for channel in user_from_db['channels']:
+                text += f"@{channel}, ".rstrip(',')
+        if user_from_db['groups'] != []:
+            text += "\nGroups: "
+            for group in user_from_db['groups']:
+                text += f"@{group}, ".rstrip(',')
+
+        await self.bot.send_message(text=text, chat_id=self.chat_id, parse_mode="HTML")
